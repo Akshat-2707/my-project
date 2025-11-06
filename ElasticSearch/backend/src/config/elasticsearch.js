6dd130f38e8dfea051fbdb2e0c602a60a7241c4a
Akshat <23BAI70667@cuchd.in>
2025-11-06 13:14:51 +0530

    Added Chatbot

--- ElasticSearch/backend/src/config/elasticsearch.js
+++
@@ -1,8 +0,0 @@
-import pkg from "elasticsearch";
-const { Client } = pkg;
-
-const client = new Client({
-  node: process.env.ELASTICSEARCH_NODE || "http://localhost:9200",
-});
-
-export default client;
